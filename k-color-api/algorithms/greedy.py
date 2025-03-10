3b8cf33da9634d08669728d2f2bbc5d7b7f7d2ca
Samuel Morgan Win10 <samuel.morgan-3@student.manchester.ac.uk>
2025-03-10 11:42:37 +0000

    add welsh powell

--- k-color-api/algorithms/greedy.py
+++ k-color-api/algorithms/greedy.py
@@ -1,3 +1,4 @@
+import random
 from .algorithmResult import algorithmResultTemplate
 from collections import deque
 
@@ -24,7 +25,7 @@ def greedy_bfs_coloring(graph, record_steps=False):
         return res_obj  # Empty graph, return empty result
 
     # Initialize BFS queue
-    bfs_queue = deque([nodes[0]])  # Start from the first node in the list
+    bfs_queue = deque([nodes[random.randint(0,len(nodes)-1)]])  # Start from a random node
     visited = set()
     visited.add(nodes[0])
 
@@ -54,7 +55,48 @@ def greedy_bfs_coloring(graph, record_steps=False):
     if record_steps:
         res_obj['steps'] = steps
     res_obj['coloring'] = coloring.copy()
+    res_obj['k'] = len(list(set(coloring.values())))
+
     return res_obj
+
+
+def welsh_powell_coloring(graph, record_steps=False):
+    """
+    Performs Welsh-Powell greedy coloring of the graph, sorting nodes by decreasing degree.
+    :param graph: dict, adjacency list of the graph
+    :param record_steps: bool, flag to record intermediate coloring steps
+    :return: dict containing 'coloring', 'k' colors used, and optional 'steps'
+    """
+    res_obj = algorithmResultTemplate.copy()
+
+    # Check for self-loops
+    for node, neighbors in graph.items():
+        if node in neighbors:
+            return None
+
+    # Sort nodes by decreasing degree
+    nodes_sorted = sorted(graph.keys(), key=lambda node: len(graph[node]), reverse=True)
+
+    coloring = {}
+
+    for node in nodes_sorted:
+        neighbor_colors = {coloring[neighbor] for neighbor in graph[node] if neighbor in coloring}
+
+        # Assign the smallest available color
+        color = 1
+        while color in neighbor_colors:
+            color += 1
+
+        coloring[node] = color
+
+        if record_steps:
+            res_obj['steps'].append(coloring.copy())
+
+    res_obj['coloring'] = coloring
+    res_obj['k'] = len(list(set(coloring.values())))
+
+    return res_obj
+
 
 
 def greedy_coloring(graph, record_steps=False):
@@ -74,6 +116,7 @@ def greedy_coloring(graph, record_steps=False):
 
     coloring = {}
     nodes = list(graph.keys())
+    random.shuffle(nodes)
     for node in nodes:
         # Get colors used by neighbors
         # TODO use sets and intersection to make this more efficeint
@@ -95,5 +138,6 @@ def greedy_coloring(graph, record_steps=False):
          
     if record_steps:
         res_obj['steps'] = steps
+    res_obj['k'] = len(list(set(coloring.values())))
     res_obj['coloring'] = coloring.copy()
     return res_obj
